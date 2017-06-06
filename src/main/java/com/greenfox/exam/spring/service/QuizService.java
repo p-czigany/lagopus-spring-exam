5c26237a896caaa9d6c2faf8f5faf62e65b5c1e8
Peter Czigany <p.czigany@gmail.com>
2017-06-06 11:49:39 +0200

    Add POST endpoint

--- src/main/java/com/greenfox/exam/spring/service/QuizService.java
+++ src/main/java/com/greenfox/exam/spring/service/QuizService.java
@@ -1,22 +1,24 @@
 package com.greenfox.exam.spring.service;
 
 import com.greenfox.exam.spring.dataaccess.RepoHandler;
+import com.greenfox.exam.spring.model.AnswerForm;
 import com.greenfox.exam.spring.model.Question;
 import com.greenfox.exam.spring.model.Quiz;
 import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 /**
  * Created by peter on 2017.06.06..
  */
 @Component
-public class QuestionService {
+public class QuizService {
 
   @Autowired
   RepoHandler repoHandler;
 
-  public Quiz chooseFive() {
+  public Quiz chooseFiveQuestions() {
     Quiz five = new Quiz();
     five.setId(1l);
     ArrayList<Question> questionBase = repoHandler.allQuestions();
@@ -28,5 +30,9 @@ public class QuestionService {
     }
     five.setQuestions(questions);
     return five;
+  }
+
+  public ResponseEntity<?> receiveAnswers(AnswerForm answerForm) {
+    return null;
   }
 }
